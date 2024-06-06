d2901a9218bced8e52277d9534d3a5a011f113b5
mdujjal67 <mdujjal.mm67@gmail.com>
2024-06-06 13:35:07 +0600

    added worksheet data on the table from database using transtack query

--- src/Pages/Dashboard/WorkSheet/WorkSheet.jsx
+++ src/Pages/Dashboard/WorkSheet/WorkSheet.jsx
@@ -3,13 +3,28 @@ import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import Swal from "sweetalert2";
 import { AuthContext } from "../../../Provider/AuthProvider";
+import useAxiosSecure from "../../../Hooks/useAxiosSecure";
+import { useQuery } from "@tanstack/react-query";
 
 const WorkSheet = () => {
 
     const [startDate, setStartDate] = useState(new Date());
 
-    const {user} = useContext(AuthContext);
-    const {email, designation} = user
+    const { user } = useContext(AuthContext);
+    const { email, designation } = user;
+
+
+    const axiosSecure = useAxiosSecure();
+
+    const { data: works = [] } = useQuery({
+        queryKey: ['users'],
+        queryFn: async () => {
+            const res = await axiosSecure.get('/workSheet')
+            return res.data
+        }
+    });
+
+
 
     const handleContactedUser = (event) => {
         event.preventDefault();
@@ -18,7 +33,7 @@ const WorkSheet = () => {
         const hoursWorked = form.hoursWorked.value;
         const date = form.date.value;
 
-        const workSheet = { taskName, hoursWorked, date, email, designation};
+        const workSheet = { taskName, hoursWorked, date, email, designation };
         console.log(workSheet);
 
         // send works data to the server 
@@ -47,83 +62,75 @@ const WorkSheet = () => {
     }
 
     return (
-        <div className="">
-            <div className="overflow-x-auto my-20">
+        <div className="container mx-auto">
+            <h1 className="text-3xl mt-8 font-bold">Your Work-Sheet:</h1>
+            <div className="overflow-x-auto my-10">
                 <table className="table">
                     {/* head */}
                     <thead>
-                        <tr>
+                        <tr className="bg-base-300">
                             <th></th>
-                            <th>Name</th>
-                            <th>Job</th>
-                            <th>Favorite Color</th>
+                            <th>Task Name</th>
+                            <th>Hours Worked</th>
+                            <th>Date</th>
                         </tr>
                     </thead>
                     <tbody>
                         {/* row 1 */}
-                        <tr className="bg-base-200">
-                            <th>1</th>
-                            <td>Cy Ganderton</td>
-                            <td>Quality Control Specialist</td>
-                            <td>Blue</td>
-                        </tr>
-                        {/* row 2 */}
-                        <tr>
-                            <th>2</th>
-                            <td>Hart Hagerty</td>
-                            <td>Desktop Support Technician</td>
-                            <td>Purple</td>
-                        </tr>
-                        {/* row 3 */}
-                        <tr>
-                            <th>3</th>
-                            <td>Brice Swyre</td>
-                            <td>Tax Accountant</td>
-                            <td>Red</td>
-                        </tr>
+                        {
+                            works.map((work, index) => <tr key={work._id} className="bg-base-100">
+                            <th>{index+1}</th>
+                            <td>{work.taskName}</td>
+                            <td>{work.hoursWorked}</td>
+                            <td>{work.date}</td>
+                        </tr>)
+                        }
                     </tbody>
                 </table>
             </div>
 
             {/* This part is for Form section */}
-            <form onSubmit={handleContactedUser} className="lg:flex lg:gap-3 ">
-                {/* This is for Tasks field */}
-                <div className="form-control">
-                    <label className="label">
-                        <span className="label-text">Your Tasks</span>
-                    </label>
-                    <select name="taskName" required className="select select-bordered focus:ring focus:ring-blue-500 focus:border-blue-500 active:border-blue-500" >
-                        <option disabled selected>Tasks</option>
-                        <option value="Sales">Sales</option>
-                        <option value="Content">Content</option>
-                        <option value="Support">Support</option>
-                        <option value="Paper-Work">Paper-Work</option>
-                    </select>
-                    {/* {errors.role && <span className="text-red-500">Role is required</span>} */}
-                </div>
+            <h2 className="text-2xl font-bold mb-5">Submit Your Works:</h2>
+            <div className="">
+                <form onSubmit={handleContactedUser} className="lg:flex lg:gap-3">
+                    {/* This is for Tasks field */}
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Your Tasks</span>
+                        </label>
+                        <select name="taskName" required className="select select-bordered focus:ring focus:ring-blue-500 focus:border-blue-500 active:border-blue-500" >
+                            <option disabled selected>Select Your Task</option>
+                            <option value="Sales">Sales</option>
+                            <option value="Content">Content</option>
+                            <option value="Support">Support</option>
+                            <option value="Paper-Work">Paper-Work</option>
+                        </select>
+                        {/* {errors.role && <span className="text-red-500">Role is required</span>} */}
+                    </div>
 
-                {/* This is for hour worked field*/}
-                <div className="form-control">
-                    <label className="label">
-                        <span className="label-text">Hours Worked</span>
-                    </label>
-                    <input  type="number" name="hoursWorked" required placeholder="Hours Worked" className="input input-bordered focus:ring focus:ring-blue-500 focus:border-blue-500 active:border-blue-500" />
-                    {/* {errors.bankAccountNumber && <span className="text-red-500">Bank Account Number is required</span>} */}
-                </div>
+                    {/* This is for hour worked field*/}
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Hours Worked</span>
+                        </label>
+                        <input type="number" name="hoursWorked" required placeholder="Hours Worked" className="input input-bordered focus:ring focus:ring-blue-500 focus:border-blue-500 active:border-blue-500" />
+                        {/* {errors.bankAccountNumber && <span className="text-red-500">Bank Account Number is required</span>} */}
+                    </div>
 
-                {/* This is for date field*/}
-                <div className="form-control">
-                    <label className="label">
-                        <span className="label-text">Date</span>
-                    </label>
-                    <DatePicker name="date" className="border border-gray-300 py-3 rounded-lg pl-5 focus:ring focus:ring-blue-500 focus:border-blue-500 active:border-blue-500" selected={startDate} onChange={(date) => setStartDate(date)} required />
-                    {/* {errors.salary && <span className="text-red-500">Salary is required</span>} */}
-                </div>
+                    {/* This is for date field*/}
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Date</span>
+                        </label>
+                        <DatePicker name="date" className="border border-gray-300 py-3 rounded-lg pl-5 focus:ring focus:ring-blue-500 focus:border-blue-500 active:border-blue-500" selected={startDate} onChange={(date) => setStartDate(date)} required />
+                        {/* {errors.salary && <span className="text-red-500">Salary is required</span>} */}
+                    </div>
 
-                <div className="form-control mt-[35px]">
-                    <input type="submit" className="btn bg-[#00a1ea] text-white w-full " value="Submit" />
-                </div>
-            </form>
+                    <div className="form-control mt-[35px]">
+                        <input type="submit" className="btn bg-[#00a1ea] text-white w-full " value="Submit" />
+                    </div>
+                </form>
+            </div>
         </div>
     );
 };
